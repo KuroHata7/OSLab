8471664bd60012c8e5b1aab5202644f0a70b5c9a
Jaffar Totanji <jaafarti@gmail.com>
2021-09-14 23:15:45 +0300

    updated exercise 4

--- week04/ex4.c
+++ week04/ex4.c
@@ -1,16 +1,36 @@
-#include<stdio.h>
-#include<stdbool.h>
-#include<string.h>
-int main(){
-  char *c[256];
-  while(true) {
-    fgets(c, 256, stdin);
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
-    if(!strcmp("exit\n",c))
-      break;
-    
-    fork();
-    system(c);
-  }
-  return 0;
+int main()
+{   
+    while(1)
+    {
+        char s[256];
+        fgets(s, 250 , stdin);
+        char ret[100][100];
+        char *pret[100];
+        int j = 0;
+        int cur = 0 ;
+        for (int i=0;i<strlen(s);i++)
+        {
+            if (s[i] == ' ' || s[i] == '\n')
+            {   
+                pret[j] = ret[j];
+                j++;
+                cur = 0;
+            }
+            else
+            {
+                ret[j][cur++] = s[i];
+                ret[j][cur] = '\0';
+            }
+        }
+        pret[j] = NULL;
+        int x = fork();
+        if(x == 0)
+            execvp(pret[0], pret);
+
+    }
+    return 0;
 }
